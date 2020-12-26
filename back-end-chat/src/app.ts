3d4cb40558b8150e4d663ed8ab1990a9d22d09b0
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-26 21:49:00 +0900

    back-end connection database

--- back-end-chat/src/app.ts
+++ back-end-chat/src/app.ts
@@ -2,10 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { Server } from "http";
+import { createConnection } from "typeorm";
 import webSocket from "./socket";
 import apitRouter from "./routes";
+import connectionOptions from "./database";
 
 const app: express.Application = express();
+
+createConnection(connectionOptions)
+.then(async (connection) => {
+  await connection.query("CREATE DATABASE IF NOT EXISTS `docker`")
+  console.log("DB connection success");
+})
+.catch(console.error);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
