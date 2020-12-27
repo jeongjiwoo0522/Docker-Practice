2e202c95b16bc0c83926df81e33100aad46ea982
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-28 08:56:30 +0900

    back-end add post api
    sucess

--- back-end-post/addPost.py
+++ back-end-post/addPost.py
@@ -1,9 +1,14 @@
-from flask import Response, request
+from flask import jsonify, request
 from app import mysql 
 
 def service():
   post = request.json["post"]
   aggent = request.json["aggent"]
-  print(post)
-  print(aggent)
-  return Response("ok")
+  sqlQuery = "INSERT INTO post (aggent, post) VALUES ('%s', '%s')" % (aggent, post)
+  conn = mysql.connect()
+  cursor = conn.cursor()
+  cursor.execute(sqlQuery)
+  conn.commit()
+  response = jsonify("Post added successfully")
+  response.status_code = 201
+  return response
