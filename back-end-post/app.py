22bf1b8a2ff3c57c5ce2bdffd64049a17ddefd5a
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-27 22:39:00 +0900

    back-end create app.py

--- back-end-post/app.py
+++ back-end-post/app.py
@@ -2,55 +2,20 @@ from flask import Flask
 from flask_cors import CORS
 from flaskext.mysql import MySQL
 
+# database config 
 from production import *
 
+# flask app
 app = Flask(__name__)
+# app cors 
 CORS(app)
+# mysql orm 
 mysql = MySQL()
 
+# database name, host, password setting
 app.config["MYSQL_DATABASE_USER"] = DATABASE_USER
 app.config["MYSQL_DATABASE_PASSWORD"] = DATABASE_PASSWORD
 app.config["MYSQL_DATABASE_DB"] = DATABASE_NAME
 app.config["MYSQL_DATABASE_HOST"] = DATABASE_HOST
 
 mysql.init_app(app)
-
-@app.route("/")
-def hellp_world():
-  return "hello"
-
-app.config["MYSQL_DATABASE_USER"] = DATABASE_USER
-# app.config["MYSQL_DATABASE_PASSWORD"] = DATABASE_PASSWORD
-# app.config["MYSQL_DATABASE_DB"] = DATABASE_NAME
-# app.config["MYSQL_DATABASE_HOST"] = DATABASE_HOST
-# app.config["MYSQL_DATABASE_USER"] = DATABASE_USER
-# app.config["MYSQL_DATABASE_PASSWORD"] = DATABASE_PASSWORD
-# app.config["MYSQL_DATABASE_DB"] = DATABASE_NAME
-# app.config["MYSQL_DATABASE_HOST"] = DATABASE_HOST
-# app.config["MYSQL_DATABASE_USER"] = DATABASE_USER
-# app.config["MYSQL_DATABASE_PASSWORD"] = DATABASE_PASSWORD
-# app.config["MYSQL_DATABASE_DB"] = DATABASE_NAME
-# app.config["MYSQL_DATABASE_HOST"] = DATABASE_HOST
-# app.config["MYSQL_DATABASE_USER"] = DATABASE_USER
-# app.config["MYSQL_DATABASE_PASSWORD"] = DATABASE_PASSWORD
-# app.config["MYSQL_DATABASE_DB"] = DATABASE_NAME
-# app.config["MYSQL_DATABASE_HOST"] = DATABASE_HOST
-# app.config["MYSQL_DATABASE_USER"] = DATABASE_USER
-# app.config["MYSQL_DATABASE_PASSWORD"] = DATABASE_PASSWORD
-# app.config["MYSQL_DATABASE_DB"] = DATABASE_NAME
-# app.config["MYSQL_DATABASE_HOST"] = DATABASE_HOST
-# app.config["MYSQL_DATABASE_USER"] = DATABASE_USER
-# app.config["MYSQL_DATABASE_PASSWORD"] = DATABASE_PASSWORD
-# app.config["MYSQL_DATABASE_DB"] = DATABASE_NAME
-# app.config["MYSQL_DATABASE_HOST"] = DATABASE_HOST
-# app.config["MYSQL_DATABASE_USER"] = DATABASE_USER
-# app.config["MYSQL_DATABASE_PASSWORD"] = DATABASE_PASSWORD
-# app.config["MYSQL_DATABASE_DB"] = DATABASE_NAME
-# app.config["MYSQL_DATABASE_HOST"] = DATABASE_HOST
-# app.config["MYSQL_DATABASE_USER"] = DATABASE_USER
-# app.config["MYSQL_DATABASE_PASSWORD"] = DATABASE_PASSWORD
-# app.config["MYSQL_DATABASE_DB"] = DATABASE_NAME
-# app.config["MYSQL_DATABASE_HOST"] = DATABASE_HOST
-
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000)
