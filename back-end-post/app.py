fab4d4867143c0a9ff40ff3bbe0b67bff39c0384
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-27 20:54:41 +0900

    back-end post

--- back-end-post/app.py
+++ back-end-post/app.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from flask_cors import CORS
+from flaskext.mysql import MySQL
+
+from production import *
+
 app = Flask(__name__)
+CORS(app)
+mysql = MySQL()
 
-from view import bp
+app.config["MYSQL_DATABASE_USER"] = DATABASE_USER
+app.config["MYSQL_DATABASE_PASSWORD"] = DATABASE_PASSWORD
+app.config["MYSQL_DATABASE_DB"] = DATABASE_NAME
+app.config["MYSQL_DATABASE_HOST"] = DATABASE_HOST
 
-app.register_blueprint(bp)
+mysql.init_app(app)
+
+@app.route("/")
+def hellp_world():
+  return "hello"
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000)
