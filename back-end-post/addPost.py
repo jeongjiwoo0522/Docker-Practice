2b662fea40f169c9ed555e0b668dd83648b166d5
jeongjiwoo0522 <jiwoourty@gmail.com>
2020-12-28 09:07:09 +0900

    back-end update add post api
    add error hadler

--- back-end-post/addPost.py
+++ back-end-post/addPost.py
@@ -2,13 +2,19 @@ from flask import jsonify, request
 from app import mysql 
 
 def service():
-  post = request.json["post"]
-  aggent = request.json["aggent"]
-  sqlQuery = "INSERT INTO post (aggent, post) VALUES ('%s', '%s')" % (aggent, post)
-  conn = mysql.connect()
-  cursor = conn.cursor()
-  cursor.execute(sqlQuery)
-  conn.commit()
-  response = jsonify("Post added successfully")
-  response.status_code = 201
-  return response
+  try:
+    post = request.json["post"]
+    aggent = request.json["aggent"]
+    print(post)
+    print(aggent)
+    sqlQuery = "INSERT INTO post (aggent, post) VALUES ('%s', '%s')" % (aggent, post)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sqlQuery)
+    conn.commit()
+    response = jsonify({ "message": "Post added successfully" }) # response message 
+    response.status_code = 201 # status code 
+    return response
+  finally:
+    conn.close()
+    cursor.close()
